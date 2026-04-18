0a00563772529182ada5c64e348a835efd57a1cd
JosiahParry <josiah.parry@gmail.com>
2026-04-18 22:45:26 +0200

    try getting sleep test workign

--- src/lib.rs
+++ src/lib.rs
@@ -8,64 +8,95 @@ pub use executor::Crostini;
 use nix::{
     errno::Errno,
     sys::{
-        signal::{SigSet, SigmaskHow, Signal, kill, sigprocmask},
+        signal::{SigSet, SigmaskHow, Signal, killpg, sigprocmask},
         signalfd::{SfdFlags, SignalFd},
         wait::{WaitPidFlag, WaitStatus, waitpid},
     },
     unistd::Pid,
 };
-use std::{ffi::OsStr, mem, process::Command};
+use std::{ffi::OsStr, os::unix::process::CommandExt, process::Command};
 
+#[tracing::instrument(skip(argv))]
 pub fn run<S: AsRef<OsStr>>(argv: &[S]) -> i32 {
-    let mut mask = SigSet::empty();
-    mask.add(Signal::SIGTERM);
-    mask.add(Signal::SIGINT);
-    mask.add(Signal::SIGCHLD);
-    sigprocmask(SigmaskHow::SIG_BLOCK, Some(&mask), None).expect("sigprocmask failed");
+    tracing::info!("crostini: starting as PID 1 init");
 
-    let sfd = SignalFd::with_flags(&mask, SfdFlags::SFD_CLOEXEC).expect("signalfd failed");
+    tracing::info!(cmd = ?argv[0].as_ref(), "crostini: spawning child");
 
+    // Spawn before blocking signals so the child inherits a clean signal mask.
     let child = Command::new(&argv[0])
         .args(&argv[1..])
+        .process_group(0)
         .spawn()
         .unwrap_or_else(|e| {
-            eprintln!("crostini: failed to spawn {:?}: {}", argv[0].as_ref(), e);
+            tracing::error!(cmd = ?argv[0].as_ref(), error = %e, "crostini: failed to spawn child");
             std::process::exit(127);
         });
 
     let child_pid = Pid::from_raw(child.id() as i32);
+    tracing::info!(%child_pid, "crostini: child spawned");
+
+    let mut mask = SigSet::all();
+    mask.remove(Signal::SIGKILL);
+    mask.remove(Signal::SIGSTOP);
+
+    // also remove fatal signals (see earlier note)
+    mask.remove(Signal::SIGSEGV);
+    mask.remove(Signal::SIGILL);
+    mask.remove(Signal::SIGBUS);
+    mask.remove(Signal::SIGABRT);
+    mask.remove(Signal::SIGFPE);
+    mask.remove(Signal::SIGTRAP);
+    mask.remove(Signal::SIGSYS);
+
+    // and TTY stop signals
+    mask.remove(Signal::SIGTTIN);
+    mask.remove(Signal::SIGTTOU);
+
+    sigprocmask(SigmaskHow::SIG_SETMASK, Some(&mask), None).unwrap();
+    // let sfd = SignalFd::with_flags(&mask, SfdFlags::SFD_CLOEXEC)?;
+    let sfd = SignalFd::with_flags(&mask, SfdFlags::SFD_CLOEXEC).expect("signalfd failed");
 
     let exit_code = 'outer: loop {
         let info = match sfd.read_signal() {
             Ok(Some(i)) => i,
             Ok(None) => continue,
             Err(e) => {
-                eprintln!("crostini: signalfd read error: {}", e);
+                tracing::error!(error = %e, "crostini: signalfd read error");
                 std::process::exit(1);
             }
         };
 
-        match Signal::try_from(info.ssi_signo as i32).unwrap() {
-            Signal::SIGTERM | Signal::SIGINT => {
-                let sig = Signal::try_from(info.ssi_signo as i32).unwrap();
-                let _ = kill(child_pid, sig);
-            }
+        let sig = match Signal::try_from(info.ssi_signo as i32) {
+            Ok(s) => s,
+            Err(_) => continue,
+        };
+
+        match sig {
             Signal::SIGCHLD => loop {
                 match waitpid(Pid::from_raw(-1), Some(WaitPidFlag::WNOHANG)) {
-                    Ok(WaitStatus::Exited(pid, code)) if pid == child_pid => break 'outer code,
+                    Ok(WaitStatus::Exited(pid, code)) if pid == child_pid => {
+                        tracing::info!(%child_pid, %code, "crostini: child exited");
+                        break 'outer code;
+                    }
                     Ok(WaitStatus::Signaled(pid, sig, _)) if pid == child_pid => {
+                        tracing::info!(%child_pid, %sig, "crostini: child killed by signal");
                         break 'outer 128 + sig as i32;
                     }
-                    Ok(WaitStatus::StillAlive) => break,
+                    Ok(WaitStatus::StillAlive) | Err(Errno::ECHILD) => break,
                     Err(Errno::EINTR) => continue,
-                    Err(Errno::ECHILD) => break,
-                    Err(_) | Ok(_) => break,
+                    Ok(_) => continue,
+                    Err(_) => break,
                 }
             },
-            _ => {}
+            Signal::SIGKILL | Signal::SIGSTOP => {}
+            sig => {
+                tracing::info!(%sig, %child_pid, "crostini: forwarding signal to child process group");
+                let _ = killpg(child_pid, sig);
+            }
         }
     };
 
+    let _ = killpg(child_pid, Signal::SIGTERM);
     loop {
         match waitpid(Pid::from_raw(-1), Some(WaitPidFlag::WNOHANG)) {
             Ok(WaitStatus::StillAlive) | Err(_) => break,
@@ -73,6 +104,5 @@ pub fn run<S: AsRef<OsStr>>(argv: &[S]) -> i32 {
         }
     }
 
-    mem::forget(child);
     exit_code
 }
---
+++ tests/sigterm.rs
@@ -0,0 +1,115 @@
+#![cfg(feature = "libcontainer")]
+
+use std::fs::create_dir_all;
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::path::Path;
+
+use anyhow::Result;
+use libcontainer::container::builder::ContainerBuilder;
+use libcontainer::oci_spec::runtime::{MountBuilder, Spec};
+use libcontainer::syscall::syscall::SyscallType;
+use nix::sys::signal::{Signal, kill};
+use nix::sys::wait::{WaitPidFlag, WaitStatus, waitpid};
+use nix::unistd::{Pid, getegid, geteuid};
+use serial_test::serial;
+use tempfile::tempdir;
+
+fn hash(v: impl Hash) -> u64 {
+    let mut hasher = DefaultHasher::default();
+    v.hash(&mut hasher);
+    hasher.finish()
+}
+
+fn use_systemd() -> bool {
+    let systemd_running = Path::new("/run/systemd/system").exists()
+        && std::fs::read_to_string("/proc/1/comm")
+            .map(|c| c.trim() == "systemd")
+            .unwrap_or(false);
+    systemd_running && std::env::var("DBUS_SESSION_BUS_ADDRESS").is_ok()
+}
+
+fn prepare_bundle(bundle: &Path) -> Result<()> {
+    let rootfs = bundle.join("rootfs");
+
+    for dir in [
+        "bin", "lib", "lib64", "usr", "proc", "sys", "dev", "tmp", "run",
+    ] {
+        create_dir_all(rootfs.join(dir))?;
+    }
+
+    let uid = geteuid().as_raw();
+    let gid = getegid().as_raw();
+
+    let mut spec = Spec::rootless(uid, gid);
+
+    if let Some(process) = spec.process_mut() {
+        process.set_args(Some(vec!["sleep".to_string(), "30".to_string()]));
+        process.set_cwd("/".into());
+    }
+
+    let mut mounts = spec.mounts().clone().unwrap_or_default();
+    for path in ["/bin", "/lib", "/usr"] {
+        if Path::new(path).exists() {
+            mounts.push(
+                MountBuilder::default()
+                    .destination(path)
+                    .typ("bind")
+                    .source(path)
+                    .options(vec!["bind".to_string(), "ro".to_string()])
+                    .build()?,
+            );
+        }
+    }
+    if Path::new("/lib64").exists() {
+        mounts.push(
+            MountBuilder::default()
+                .destination("/lib64")
+                .typ("bind")
+                .source("/lib64")
+                .options(vec!["bind".to_string(), "ro".to_string()])
+                .build()?,
+        );
+    }
+    spec.set_mounts(Some(mounts));
+
+    spec.save(bundle.join("config.json"))?;
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn sigterm_forwarded_to_child() -> Result<()> {
+    let root = tempdir()?;
+    let bundle = root.path().join("bundle");
+    let state = root.path().join("state");
+    create_dir_all(&state)?;
+
+    let id = format!("crostini-test-{:x}", hash(root.path()));
+    prepare_bundle(&bundle)?;
+
+    let container = ContainerBuilder::new(id, SyscallType::Linux)
+        .with_executor(crostini::Crostini)
+        .with_root_path(&state)?
+        .as_init(&bundle)
+        .with_systemd(use_systemd())
+        .build()?;
+
+    let init_pid = Pid::from_raw(container.pid().unwrap().as_raw());
+
+    let container = scopeguard::guard(container, |mut c| {
+        let _ = c.delete(true);
+    });
+
+    // Give crostini time to spawn its child before sending SIGTERM.
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    kill(init_pid, Signal::SIGTERM)?;
+
+    let status = waitpid(init_pid, Some(WaitPidFlag::empty()))?;
+
+    match status {
+        WaitStatus::Exited(_, code) => assert_eq!(code, 143),
+        other => panic!("unexpected wait status: {other:?}"),
+    }
+
+    Ok(())
+}
