fdd593186021cb4668a19383447253a625bf605a
Josiah Parry <josiah.parry@gmail.com>
2026-09-26 16:51:35 +0000

    undo my changes to the tests

--- src/executor.rs
+++ src/executor.rs
@@ -108,9 +108,9 @@ mod tests {
         let before: HashMap<String, String> = std::env::vars().collect();
         let envs = HashMap::from([("CROSTINI_TEST_VAR".to_string(), "set".to_string())]);
 
-        let _ = Crostini.setup_envs(envs);
+        assert!(Crostini.setup_envs(envs.clone()).is_ok());
 
         assert_eq!(std::env::vars().collect::<HashMap<_, _>>(), before);
-        assert!(CONTAINER_ENVS.get().is_some());
+        assert_eq!(CONTAINER_ENVS.get(), Some(&envs));
     }
 }
